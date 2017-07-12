21d96d71b8b330804259d708e81c95019af7bd72
Ryan Mack <ryanjmack21@gmail.com>
2017-07-12 11:22:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# frontend-project-template
+
+## Getting Started
+* Clone this repository
+* Use`npm install` to get all required node modules
+* Use `gulp` to run default task
+* Start coding
+
+## Basics
+The folder `src` is the development build
+
+The folder `build` is the production build (minified/concatenated files etc)
+
+## List of gulp tasks
+* `gulp styles` converts scss to css in `src` folder; also sets up sourcemaps for scss debugging
+* `gulp serve` creates a webserver for browsersync which watches for any html/scss changes
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('styles', function() {
+    gulp.src('./src/stylesheets/sass/master.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./src/stylesheets/css'))
+        .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: './src'
+        }
+    });
+
+    gulp.watch('./src/stylesheets/sass/*.scss', ['styles']);
+    gulp.watch('./src/*.html').on('change', browserSync.reload);
+
+});
+
+gulp.task('default', ['styles', 'serve']);
