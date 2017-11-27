d04b3b0ffbbc87f4798267efb08787f7db2d9006
Ryan Mack <ryanjmack21@gmail.com>
2017-11-27 12:16:27 -0500

    Create calculator object with some methods; it now works for addition

--- src/js/general.js
+++ src/js/general.js
@@ -1,47 +1,101 @@
+const calculator = {
+  // data members
+  total: null,
+  firstNum: true,
+  currentInput: '',
+  stringOutput: '',
+  currentOperation: null,
 
-// this grabs the whole calculator; used for event delegation (click)
-const calculator = document.querySelector('main');
+/***********************************************************************
+** calculator methods
+***********************************************************************/
+  add: function(x) {
+    this.total += x;
+  },
+
+  resetCurrentInput: function() {
+    this.currentInput = '';
+  },
+
+  updateCurrentInput: function(val) {
+    this.currentInput += val;
+    this.updateStringOutput(val);
+  },
+
+
+  updateStringOutput: function(val) {
+    this.stringOutput += val;
+    updateView(this.stringOutput);
+  },
+
+  // simple operators: +, -, *, /, %
+  handleOperator: function(value, operator) {
+    if (this.firstNum) {
+      this.firstNum = false;
+      this.total = parseFloat(this.currentInput);
+      this.currentInput = '';
+    }
+    else if (this.currentInput === '') {
+      return;
+    }
+    else {
+      this.currentOperation(parseFloat(this.currentInput));
+      this.resetCurrentInput();
+    }
+    console.log(this.total);
+    this.updateStringOutput(` ${operator} `);
+    this.currentOperation = this[value];
+  },
+
+  // special operators: AC, =, (-)
+  handleSpecialOperators: function(value, operator) {
+    if (value === "equals") {
+      this.currentOperation(parseFloat(this.currentInput));
+      updateView(this.total);
+    }
+  }
+}
+
 
 
 /***********************************************************************
-* Runs when the user clicks or
+** Runs when the user clicks any buttons
 ***********************************************************************/
 function handleClick(e) {
   e.preventDefault();
   const buttonValue = e.target.dataset.value;
+  const buttonOperator = e.target.dataset.operator;
 
-  switch (buttonValue) {
-    case 'AC':
-      console.log("AC");
-      break;
-    case 'negative':
-      console.log('neg');
-      break;
-    case 'percent':
-      console.log('percent');
-      break;
-    case 'divide':
-      console.log('divide');
-      break;
-    case 'multiply':
-      console.log('multiply');
-      break;
-    case 'subtract':
-      console.log('sub');
-      break;
-    case 'add':
-      console.log('add');
-      break;
-    case 'equals':
-      console.log('equals');
-      break;
-    case '.':
-      console.log('decimal');
-      break;
-  default:
-      console.log('Number');
-      break;
+  if (!buttonValue) { // all buttons have a buttonValue
+    console.error("Element does not have dataset-value attribute");
+    return;
+  }
+  else if (!buttonOperator) { // If it's not an operator, it's a number
+    calculator.updateCurrentInput(Number(buttonValue));
+  }
+  else { // we are dealing with an operator of some sort
+    switch (buttonValue) {
+      // these are special operators
+      case 'AC':
+      case 'negative':
+      case 'equals':
+        calculator.handleSpecialOperators(buttonValue, buttonOperator);
+        break;
+      default:
+        calculator.handleOperator(buttonValue, buttonOperator);
+    }
   }
 }
 
-calculator.addEventListener('click', handleClick);
+
+/***********************************************************************
+** Updates the view of the calculator every time a button is pressd
+***********************************************************************/
+function updateView(str) {
+  document.querySelector('.output-field p').innerText = str;
+}
+
+/***********************************************************************
+** Event listener for the calculator object
+***********************************************************************/
+document.querySelector('main').addEventListener('click', handleClick);
