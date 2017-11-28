203c4fa103334c9da212d2fa839d946d76794072
Ryan Mack <ryanjmack21@gmail.com>
2017-11-28 09:15:03 -0500

    Fixing some minor bugs and making calculator encapsulated (unfinished)

--- src/js/general.js
+++ src/js/general.js
@@ -19,7 +19,68 @@ const calculator = {
     return function(num) {
       return this.total + num;
     }
+  },
+
+  subtract: function() {
+    return function(num) {
+      return this.total - num;
+    }
+  },
+
+  multiply: function() {
+    return function(num) {
+      return this.total * num;
+    }
+  },
+
+  divide: function() {
+    return function(num) {
+      return this.total / num;
+    }
+  },
+
+  /* percentage: function() {
+    return function(num) {
+      return this.total + num;
+    }
+  },*/
+  reset: function(hardReset) {
+    // hard reset is used when "AC" is pressed
+    if (hardReset) {
+      calculator.total=  null;
+      calculator.currentInput= '';
+      calculator.stringOutput = '';
+    }
+
+    // soft reset, used when user presses 'equal'
+    calculator.nextNumNegative = false;
+    calculator.firstNum = true;
+    calculator.currentOperation = null;
+  },
+
+  allClear: function() {
+    // hard reset
+    calculator.reset(true);
+  },
+
+  equals: function() {
+    console.log('equals this value is: ', this);
+    if (!this.currentOperation)
+      return;
+    // find out how much precision we want to dislay - avoids floating point problems
+    const precision = (() => {
+      return Math.max(calculator.currentInput.match(/(?!\d)\.$/)||''.length,
+               String(calculator.total).match(/(?!\d)\.$/)||''.length);
+    })();
+
+    this.total = parseFloat(this.currentOperation(parseFloat(this.currentInput)).toFixed(precision));
+    this.currentInput = `${this.total}`;
+    this.stringOutput = `${this.total}`;
+
+    // soft reset
+    this.reset(false);
   }
+
 } // end calculator object
 
 
@@ -27,6 +88,7 @@ const calculator = {
 ** Runs when the user clicks any buttons
 ***********************************************************************/
 function handleClick(e) {
+  // get the dataset information
   e.preventDefault();
   const buttonValue = e.target.dataset.value;
   const buttonOperator = e.target.dataset.operator;
@@ -36,15 +98,25 @@ function handleClick(e) {
     return;
   }
   else if (!buttonOperator) { // If it's not an operator, it's a number
-    calculator.currentInput += buttonValue;
-    calculator.stringOutput += buttonValue;
+    if (calculator.firstNum && calculator.nextNumNegative) {
+      calculator.currentInput += `-${buttonValue}`;
+      calculator.stringOutput += `-${buttonValue}`;
+    }
+    else {
+      calculator.currentInput += buttonValue;
+      calculator.stringOutput += buttonValue;
+    }
   }
   else { // we are dealing with an operator of some sort
     switch (buttonValue) {
       // these are special operators
       case 'AC':
-      case 'negative':
+        calculator.allClear();
+        break;
       case 'equals':
+        calculator.equals();
+        break;
+      case 'negative':
       case 'decimal':
         handleSpecialOperators(buttonValue, buttonOperator);
         break;
@@ -57,18 +129,19 @@ function handleClick(e) {
 }
 
 function handleOperator(value, operator) {
+  if (!calculator.currentInput)
+    return;
+
   // first number using is inputting
   if (calculator.firstNum) {
     calculator.firstNum = false;
-    calculator.total = parseFloat(calculator.currentInput);
+    calculator.total = calculator.total || parseFloat(calculator.currentInput);
   }
   else { // not the first number user is inputting
     calculator.total = calculator.currentOperation(parseFloat(calculator.currentInput));
   }
 
-    calculator.currentOperation = calculator[value]();
-
-
+  calculator.currentOperation = calculator[value]();
   calculator.currentInput = '';
   calculator.stringOutput += ` ${operator} `
 }
@@ -79,11 +152,15 @@ function handleSpecialOperators(value, operator) {
 
     if (calculator.nextNumNegative === false) {
       calculator.currentInput = calculator.currentInput.replace(/^\-/, '');
-      calculator.stringOutput = calculator.stringOutput.replace(/\-(\d*.?\d*)$/, '$1');
+      calculator.stringOutput = calculator.stringOutput.replace(/\-(\d+(.?\d)*)$/, '$1');
+    }
+    else if (calculator.total && calculator.firstNum) {
+      calculator.total *= -1;
+      calculator.stringOutput = calculator.stringOutput.replace(/\-(\d+(.?\d)*)$/, '$1');
     }
     else {
       calculator.currentInput = `-${calculator.currentInput}`;
-      calculator.stringOutput = calculator.stringOutput.replace(/\s(\d*\.?\d*)$/, ` -$1`);
+      calculator.stringOutput = calculator.stringOutput.replace(/\s?(\d*\.?\d*)?$/, ` -$1`);
     }
   }
   else if (value === "decimal") {
@@ -91,31 +168,9 @@ function handleSpecialOperators(value, operator) {
       return;
 
     // format the decimal
-    const decimal = (calculator.currentInput) ? '.' : '0.';
+    const decimal = (calculator.currentInput && calculator.currentInput !== "-") ? '.' : '0.';
     calculator.currentInput += decimal;
     calculator.stringOutput += decimal;
-  }
-  else {
-    if (value === "equals") {
-      // find out how much precision we want to dislay - avoids floating point problems
-      const precision = (() => {
-        return Math.max(calculator.currentInput.match(/(?!\d)\.$/)||''.length,
-                 String(calculator.total).match(/(?!\d)\.$/)||''.length);
-      })();
-
-      calculator.total = calculator.currentOperation(parseFloat(calculator.currentInput)).toFixed(precision);
-      calculator.currentInput = `${calculator.total}`;
-      calculator.stringOutput = `${calculator.total}`;
-    }
-    else { // "AC" button
-      calculator.total=  null;
-      calculator.currentInput= '';
-      calculator.stringOutput = '';
-    }
-    calculator.nextNumNegative = false;
-    calculator.firstNum = true;
-    calculator.currentOperation = null;
-
   }
 }
 
