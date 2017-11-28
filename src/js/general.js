67625bbcb8769857d9c817b8a2c9d216056e257c
Ryan Mack <ryanjmack21@gmail.com>
2017-11-28 09:16:48 -0500

    Reduce code verbosity by replacing calculator with calc

--- src/js/general.js
+++ src/js/general.js
@@ -2,11 +2,11 @@
 ** Name: Ryan Mack
 ** Date: 11/27/2017
 ** Description: File that contains the code for a simple web based
-** calculator
+** calc
 ***********************************************************************/
 
-// holds data and functions for the calculator
-const calculator = {
+// holds data and functions for the calculator object
+const calc = {
   // data members
   total: null, // running total
   firstNum: true,
@@ -47,20 +47,20 @@ const calculator = {
   reset: function(hardReset) {
     // hard reset is used when "AC" is pressed
     if (hardReset) {
-      calculator.total=  null;
-      calculator.currentInput= '';
-      calculator.stringOutput = '';
+      calc.total=  null;
+      calc.currentInput= '';
+      calc.stringOutput = '';
     }
 
     // soft reset, used when user presses 'equal'
-    calculator.nextNumNegative = false;
-    calculator.firstNum = true;
-    calculator.currentOperation = null;
+    calc.nextNumNegative = false;
+    calc.firstNum = true;
+    calc.currentOperation = null;
   },
 
   allClear: function() {
     // hard reset
-    calculator.reset(true);
+    calc.reset(true);
   },
 
   equals: function() {
@@ -69,8 +69,8 @@ const calculator = {
       return;
     // find out how much precision we want to dislay - avoids floating point problems
     const precision = (() => {
-      return Math.max(calculator.currentInput.match(/(?!\d)\.$/)||''.length,
-               String(calculator.total).match(/(?!\d)\.$/)||''.length);
+      return Math.max(calc.currentInput.match(/(?!\d)\.$/)||''.length,
+               String(calc.total).match(/(?!\d)\.$/)||''.length);
     })();
 
     this.total = parseFloat(this.currentOperation(parseFloat(this.currentInput)).toFixed(precision));
@@ -81,7 +81,7 @@ const calculator = {
     this.reset(false);
   }
 
-} // end calculator object
+} // end calc object
 
 
 /***********************************************************************
@@ -98,23 +98,23 @@ function handleClick(e) {
     return;
   }
   else if (!buttonOperator) { // If it's not an operator, it's a number
-    if (calculator.firstNum && calculator.nextNumNegative) {
-      calculator.currentInput += `-${buttonValue}`;
-      calculator.stringOutput += `-${buttonValue}`;
+    if (calc.firstNum && calc.nextNumNegative) {
+      calc.currentInput += `-${buttonValue}`;
+      calc.stringOutput += `-${buttonValue}`;
     }
     else {
-      calculator.currentInput += buttonValue;
-      calculator.stringOutput += buttonValue;
+      calc.currentInput += buttonValue;
+      calc.stringOutput += buttonValue;
     }
   }
   else { // we are dealing with an operator of some sort
     switch (buttonValue) {
       // these are special operators
       case 'AC':
-        calculator.allClear();
+        calc.allClear();
         break;
       case 'equals':
-        calculator.equals();
+        calc.equals();
         break;
       case 'negative':
       case 'decimal':
@@ -124,62 +124,62 @@ function handleClick(e) {
         handleOperator(buttonValue, buttonOperator);
     }
   }
-  console.log(calculator);
+  console.log(calc);
   updateView();
 }
 
 function handleOperator(value, operator) {
-  if (!calculator.currentInput)
+  if (!calc.currentInput)
     return;
 
   // first number using is inputting
-  if (calculator.firstNum) {
-    calculator.firstNum = false;
-    calculator.total = calculator.total || parseFloat(calculator.currentInput);
+  if (calc.firstNum) {
+    calc.firstNum = false;
+    calc.total = calc.total || parseFloat(calc.currentInput);
   }
   else { // not the first number user is inputting
-    calculator.total = calculator.currentOperation(parseFloat(calculator.currentInput));
+    calc.total = calc.currentOperation(parseFloat(calc.currentInput));
   }
 
-  calculator.currentOperation = calculator[value]();
-  calculator.currentInput = '';
-  calculator.stringOutput += ` ${operator} `
+  calc.currentOperation = calc[value]();
+  calc.currentInput = '';
+  calc.stringOutput += ` ${operator} `
 }
 
 function handleSpecialOperators(value, operator) {
   if (value === "negative") {
-    calculator.nextNumNegative = !calculator.nextNumNegative;
+    calc.nextNumNegative = !calc.nextNumNegative;
 
-    if (calculator.nextNumNegative === false) {
-      calculator.currentInput = calculator.currentInput.replace(/^\-/, '');
-      calculator.stringOutput = calculator.stringOutput.replace(/\-(\d+(.?\d)*)$/, '$1');
+    if (calc.nextNumNegative === false) {
+      calc.currentInput = calc.currentInput.replace(/^\-/, '');
+      calc.stringOutput = calc.stringOutput.replace(/\-(\d+(.?\d)*)$/, '$1');
     }
-    else if (calculator.total && calculator.firstNum) {
-      calculator.total *= -1;
-      calculator.stringOutput = calculator.stringOutput.replace(/\-(\d+(.?\d)*)$/, '$1');
+    else if (calc.total && calc.firstNum) {
+      calc.total *= -1;
+      calc.stringOutput = calc.stringOutput.replace(/\-(\d+(.?\d)*)$/, '$1');
     }
     else {
-      calculator.currentInput = `-${calculator.currentInput}`;
-      calculator.stringOutput = calculator.stringOutput.replace(/\s?(\d*\.?\d*)?$/, ` -$1`);
+      calc.currentInput = `-${calc.currentInput}`;
+      calc.stringOutput = calc.stringOutput.replace(/\s?(\d*\.?\d*)?$/, ` -$1`);
     }
   }
   else if (value === "decimal") {
-    if (calculator.currentInput.includes('.'))
+    if (calc.currentInput.includes('.'))
       return;
 
     // format the decimal
-    const decimal = (calculator.currentInput && calculator.currentInput !== "-") ? '.' : '0.';
-    calculator.currentInput += decimal;
-    calculator.stringOutput += decimal;
+    const decimal = (calc.currentInput && calc.currentInput !== "-") ? '.' : '0.';
+    calc.currentInput += decimal;
+    calc.stringOutput += decimal;
   }
 }
 
 function updateView() {
-  document.querySelector('.output-field p').innerText = calculator.stringOutput;
+  document.querySelector('.output-field p').innerText = calc.stringOutput;
 }
 
 /***********************************************************************
-** Event listener for the calculator object. Event delegation is used
+** Event listener for the calc object. Event delegation is used
 ** so we don't need 19 event listeners for all the buttons
 ***********************************************************************/
 document.querySelector('main').addEventListener('click', handleClick);
