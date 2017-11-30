cfacf561c3a0fa9f1bf69d6de579456617413927
Ryan Mack <ryanjmack21@gmail.com>
2017-11-30 12:06:32 -0500

    Add equals function and fix formatting when output is displayed

--- src/js/general.js
+++ src/js/general.js
@@ -70,6 +70,10 @@ const calc = {
     else if (this.currentInput.includes('(-)')) {
       this.currentInput = this.currentInput.slice(3);
     }
+    // after equals button is hit, if result is negative, it looks like -9, not (-)9
+    else if (/^\-\d/.test(this.currentInput)) {
+      this.currentInput = this.currentInput.slice(1);
+    }
     else { // add a negative sign
       this.currentInput = `(-)${this.currentInput}`;
     }
@@ -91,9 +95,73 @@ const calc = {
     }
   },
 
-  compute: function() {
-    console.log('computing...');
+
+  equals: function() {
+    if (this.data.length === 0) {
+      return;
+    }
+
+    // push the last entry to data
+    this.data.push(this.currentInput);
+    this.currentInput = '';
+
+    // can't compute if equation is even elements or if last element is only a negative sign
+    if (this.data.length % 2 === 0 || this.currentInput === '(-)') {
+      this.data = ['Error'];
+      return;
+    }
+
+    // clean the data --> replace (-) with '-', so parseFloat can operate on the numbers
+    this.data = this.data.map(entry => entry.replace('(-)', '-'));
+
+    // evaluate expressions following proper order of operations
+    this.evaluateExpressions('*', '/');
+    this.evaluateExpressions('+', '-');
+
+    // once statement is evaluated treat it like the user is inputting the first number
+    // clear data and convert result to string
+    this.currentInput = String(this.data.pop());
+  },
+
+  // evaluates expressions one by one, iteratively replacing expressions with values
+  evaluateExpressions: function(op1, op2) {
+
+    // saves where our next expression will start
+    let expressionStart = this.data.findIndex(val => val === op1 || val === op2) - 1;
+
+    while (expressionStart >= 0) {
+
+      const expression = [...this.data.slice(expressionStart, expressionStart + 3)];
+      const num1 = parseFloat(expression[0]);
+      const operator = expression[1];
+      const num2 = parseFloat(expression[2]);
+      let result;
+
+      if (operator === '*') {
+        result = num1 * num2;
+      }
+      else if (operator === '/') {
+        if (num2 === 0) { // division by 0
+          this.data = ['Error'];
+          return;
+        }
+        result = num1 / num2;
+      }
+      else if (operator === "+") {
+        result = num1 + num2;
+      }
+      else {
+        result = num1 - num2;
+      }
+
+      // replace the expression with the result in data
+      this.data.splice(expressionStart, 3, result);
+
+      // get the index of the start of the next * or / espression
+      expressionStart = this.data.findIndex(val => val === op1 || val === op2) - 1;
+    }
   }
+
 }  // end calc obj
 
 
@@ -102,6 +170,15 @@ const calc = {
 ***********************************************************************/
 function handleClick(e) {
   const buttonValue = e.target.dataset.value;
+
+  // user trie to calculate an invalid expression
+  if (calc.data[0] === 'Error') {
+    if (buttonValue === "CE" || buttonValue === "AC") {
+      calc.AC(); // run all clear for CE or AC when there is an error
+    }
+    updateView();
+    return; // no other buttons will work
+  }
 
   // dataset-value attribute of event is a number
   if (!buttonValue) { // not a button
@@ -125,7 +202,7 @@ function handleClick(e) {
         calc.decimal();
         break;
       case '=':
-        calc.compute();
+        calc.equals();
         break;
       default: // +, -, *, /
         calc.inputOperator(buttonValue);
@@ -140,7 +217,22 @@ function handleClick(e) {
 ** (valid button is clicked)
 ***********************************************************************/
 function updateView() {
-  document.querySelector('.output-field p').innerText = `${calc.data.join(' ')} ${calc.currentInput}`;
+  // if the
+  let output = ((calc.data.length > 0) ? calc.data.join(' ') + ' ' : '') + calc.currentInput;
+
+  // if the computation results is a large decimal, format to 4 decimal spots
+  // also remove trailing 0s at the end of a decimal
+  if (/^\-?\d*\.\d{4,}$/.test(output)) {
+    output = Number(output).toFixed(8);
+    output = output.replace(/\.(\d*)0+$/, '$1');
+    calc.currentInput = output;
+  }
+  // replace (-) with -
+  else if (output.includes('(-)')) {
+    output = output.replace(/\(\-\)/g, '-');
+  }
+
+  document.querySelector('.output-field p').innerText = output;
 }
 
 
