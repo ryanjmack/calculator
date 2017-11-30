c60472745fa01e7f992580641b5f7a5f4468f450
Ryan Mack <ryanjmack21@gmail.com>
2017-11-30 10:11:44 -0500

    Add negative (-) functionality to calculator

--- src/js/general.js
+++ src/js/general.js
@@ -18,7 +18,7 @@ const calc = {
       this.firstInput = false;
     }
     // if the current input is a number
-    else if (/\d/.test(this.currentInput)) {
+    else if (/\d/.test(this.currentInput) || this.currentInput.includes('(-)')) {
       this.currentInput += num;
     }
     // currentInput is an operator. Push it to the array, start inputting new num
@@ -33,7 +33,7 @@ const calc = {
       return;
     }
     // currentInput is an operator, swap them
-    else if (/[\-+*/]/.test(this.currentInput)) {
+    else if (/^[\-+*/]$/.test(this.currentInput)) {
       this.currentInput = op;
     }
     else {
@@ -57,6 +57,22 @@ const calc = {
     this.data = [];
     this.currentInput = '';
     this.firstInput = true;
+  },
+
+  negative: function() {
+    // currentInput is an operator
+    if (/^[\-+*/]$/.test(this.currentInput)) {
+      // push it to data and concat currentInput
+      this.data.push(this.currentInput);
+      this.currentInput = '(-)';
+    }
+    // if currentInput is negative, remove the negative sign
+    else if (this.currentInput.includes('(-)')) {
+      this.currentInput = this.currentInput.slice(3);
+    }
+    else {
+      this.currentInput = `(-)${this.currentInput}`;
+    }
   }
 }  // end calc obj
 
@@ -82,7 +98,9 @@ function handleClick(e) {
       case 'CE':
         calc.CE();
         break;
-      case 'neg':
+      case '(-)':
+        calc.negative();
+        break;
       case '=':
       case '.':
         console.log('special operator');
@@ -91,7 +109,7 @@ function handleClick(e) {
         calc.inputOperator(buttonValue);
     }
   }
-
+  console.log(calc);
   updateView();
 }
 
